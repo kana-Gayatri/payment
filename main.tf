a22849542f5413c7fe25149ccdedd32580a61860
Gayatri <gayatrikanaparthi@gmail.com>
2021-11-27 23:43:00 +0500

    repo for payment and catalogue  created

---
+++ main.tf
@@ -0,0 +1,15 @@
+module "cart" {
+  source              = "git::https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/terraform-mutable//app-module"
+  INSTANCE_TYPE       = var.INSTANCE_TYPE
+  ENV                 = var.ENV
+  SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
+  OD_INSTANCE_COUNT   = var.OD_INSTANCE_COUNT
+  COMPONENT           = "payment"
+  PORT                = 8080
+  IS_PRIVATE_LB       = true
+  LB_RULE_PRIORITY    = 103
+}
+
+output "INSTANCE_IDS" {
+  value = module.cart.INSTANCE_IDS
+}
